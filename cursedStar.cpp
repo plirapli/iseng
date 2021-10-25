8e53d3ffa4d2c71b934d595be5769e06089120a6
rafliyulistiawan <rafliyulistiawan@gmail.com>
2021-10-25 08:11:33 +0700

    [fix] fix jumlah pattern

--- cursedStar.cpp
+++ cursedStar.cpp
@@ -196,12 +196,12 @@ int main()
 	// * N with rectangle
 	for (int i = 0; i < 10; i++)
 	{
-		for (int j = 0; j <= 10; j++)
+		for (int j = 0; j < 10; j++)
 		{
-			if (i == 0 || i == 9 || j == 0 || j == 10 || i == j)
-				cout << "*";
+			if (i == 0 || i == 9 || j == 0 || j == 9 || i == j)
+				cout << "* ";
 			else
-				cout << " ";
+				cout << "  ";
 		}
 		cout << "\n";
 	}
