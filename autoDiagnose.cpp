97e40027d8c34c4f1d909144bd527f8c82b7efe5
rafliyulistiawan <rafliyulistiawan@gmail.com>
2021-11-03 00:07:40 +0700

    [patch] add feature in autoDiagnose program

--- autoDiagnose.cpp
+++ autoDiagnose.cpp
@@ -6,8 +6,7 @@ void questionSymptom(int &outJmlPertanyaan, string symptom, char &outInput);
 
 int main()
 {
-	int jmlPertanyaan = 0;
-	char inputG[17];
+	char inputG[17], ulangiBaru, ulangi;
 	string nim, tgl, nama,
 			garis = "------------------------------------------",
 			outG[17] =
@@ -36,114 +35,149 @@ int main()
 					 "[ P07 ] Milia", "[ P08 ] Acne rosacea"},
 			diagnosa = outP[0];
 
-	cout << garis << "\n";
-	cout << "No. Diagnosa \t: ";
-	getline(cin, nim);
-	cout << "Tgl. Diagnosa \t: ";
-	getline(cin, tgl);
-	cout << "Nama Pasien \t: ";
-	getline(cin, nama);
-	cout << garis << "\n\n";
-
-	string hasilDiagnosaText = "HASIL DIAGNOSA PENYAKIT JERAWAT (" + nama + "): \n";
-
-	cout << "PERTANYAAN GEJALA :";
-	questionSymptom(jmlPertanyaan, outG[15], inputG[15]);
-
-	if (inputG[15] == 'Y' || inputG[15] == 'y')
+	do
 	{
-		questionSymptom(jmlPertanyaan, outG[12], inputG[12]);
+		int jmlPertanyaan = 0;
 
-		if (inputG[12] == 'Y' || inputG[12] == 'y')
+		cout << garis << "\n";
+		cout << "No. Diagnosa \t: ";
+		getline(cin, nim);
+		cout << "Tgl. Diagnosa \t: ";
+		getline(cin, tgl);
+		cout << "Nama Pasien \t: ";
+		getline(cin, nama);
+		cout << garis << "\n\n";
+
+		string hasilDiagnosaText = "HASIL DIAGNOSA PENYAKIT JERAWAT (" + nama + "): \n";
+
+		do
 		{
-			questionSymptom(jmlPertanyaan, outG[13], inputG[13]);
-
-			if (inputG[13] == 'Y' || inputG[13] == 'y')
-				diagnosa = outP[1];
-			else
+			if (ulangiBaru == 't' || ulangiBaru == 'T')
 			{
-				questionSymptom(jmlPertanyaan, outG[14], inputG[14]);
+				cout << garis << "\n"
+						 << "No. Diagnosa \t: " + nim << "\n"
+						 << "Tgl. Diagnosa \t: " + tgl << "\n"
+						 << "Nama Pasien \t: " + nama << "\n"
+						 << garis << "\n\n";
+			}
 
-				if (inputG[14] == 'Y' || inputG[14] == 'y')
-					diagnosa = outP[2];
-				else
+			cout << "PERTANYAAN GEJALA :";
+			questionSymptom(jmlPertanyaan, outG[15], inputG[15]);
+
+			if (inputG[15] == 'Y' || inputG[15] == 'y')
+			{
+				questionSymptom(jmlPertanyaan, outG[12], inputG[12]);
+
+				if (inputG[12] == 'Y' || inputG[12] == 'y')
 				{
-					questionSymptom(jmlPertanyaan, outG[1], inputG[1]);
+					questionSymptom(jmlPertanyaan, outG[13], inputG[13]);
 
-					if (inputG[1] == 'Y' || inputG[1] == 'y')
+					if (inputG[13] == 'Y' || inputG[13] == 'y')
+						diagnosa = outP[1];
+					else
 					{
-						questionSymptom(jmlPertanyaan, outG[3], inputG[3]);
+						questionSymptom(jmlPertanyaan, outG[14], inputG[14]);
 
-						if (inputG[3] == 'Y' || inputG[3] == 'y')
-							diagnosa = outP[3];
+						if (inputG[14] == 'Y' || inputG[14] == 'y')
+							diagnosa = outP[2];
 						else
 						{
-							questionSymptom(jmlPertanyaan, outG[4], inputG[4]);
+							questionSymptom(jmlPertanyaan, outG[1], inputG[1]);
 
-							if (inputG[4] == 'Y' || inputG[4] == 'y')
+							if (inputG[1] == 'Y' || inputG[1] == 'y')
 							{
-								questionSymptom(jmlPertanyaan, outG[2], inputG[2]);
+								questionSymptom(jmlPertanyaan, outG[3], inputG[3]);
 
-								if (inputG[2] == 'Y' || inputG[2] == 'y')
-									diagnosa = outP[4];
+								if (inputG[3] == 'Y' || inputG[3] == 'y')
+									diagnosa = outP[3];
+								else
+								{
+									questionSymptom(jmlPertanyaan, outG[4], inputG[4]);
+
+									if (inputG[4] == 'Y' || inputG[4] == 'y')
+									{
+										questionSymptom(jmlPertanyaan, outG[2], inputG[2]);
+
+										if (inputG[2] == 'Y' || inputG[2] == 'y')
+											diagnosa = outP[4];
+									}
+								}
 							}
 						}
 					}
 				}
 			}
-		}
-	}
-	else
-	{
-		questionSymptom(jmlPertanyaan, outG[5], inputG[5]);
-
-		if (inputG[5] == 'Y' || inputG[5] == 'y')
-		{
-			questionSymptom(jmlPertanyaan, outG[6], inputG[6]);
-
-			if (inputG[6] == 'Y' || inputG[6] == 'y')
-				diagnosa = outP[5];
-		}
-		else
-		{
-			questionSymptom(jmlPertanyaan, outG[11], inputG[11]);
-
-			if (inputG[11] == 'Y' || inputG[11] == 'y')
-			{
-				questionSymptom(jmlPertanyaan, outG[16], inputG[16]);
-
-				if (inputG[6] == 'Y' || inputG[6] == 'y')
-					diagnosa = outP[8];
-			}
 			else
 			{
-				questionSymptom(jmlPertanyaan, outG[9], inputG[9]);
+				questionSymptom(jmlPertanyaan, outG[5], inputG[5]);
 
-				if (inputG[9] == 'Y' || inputG[9] == 'y')
+				if (inputG[5] == 'Y' || inputG[5] == 'y')
 				{
-					questionSymptom(jmlPertanyaan, outG[10], inputG[10]);
+					questionSymptom(jmlPertanyaan, outG[6], inputG[6]);
 
-					if (inputG[10] == 'Y' || inputG[10] == 'y')
-						diagnosa = outP[7];
+					if (inputG[6] == 'Y' || inputG[6] == 'y')
+						diagnosa = outP[5];
 				}
 				else
 				{
-					questionSymptom(jmlPertanyaan, outG[7], inputG[7]);
+					questionSymptom(jmlPertanyaan, outG[11], inputG[11]);
 
-					if (inputG[7] == 'Y' || inputG[7] == 'y')
+					if (inputG[11] == 'Y' || inputG[11] == 'y')
 					{
-						questionSymptom(jmlPertanyaan, outG[8], inputG[8]);
+						questionSymptom(jmlPertanyaan, outG[16], inputG[16]);
 
-						if (inputG[8] == 'Y' || inputG[8] == 'y')
-							diagnosa = outP[6];
+						if (inputG[6] == 'Y' || inputG[6] == 'y')
+							diagnosa = outP[8];
+					}
+					else
+					{
+						questionSymptom(jmlPertanyaan, outG[9], inputG[9]);
+
+						if (inputG[9] == 'Y' || inputG[9] == 'y')
+						{
+							questionSymptom(jmlPertanyaan, outG[10], inputG[10]);
+
+							if (inputG[10] == 'Y' || inputG[10] == 'y')
+								diagnosa = outP[7];
+						}
+						else
+						{
+							questionSymptom(jmlPertanyaan, outG[7], inputG[7]);
+
+							if (inputG[7] == 'Y' || inputG[7] == 'y')
+							{
+								questionSymptom(jmlPertanyaan, outG[8], inputG[8]);
+
+								if (inputG[8] == 'Y' || inputG[8] == 'y')
+									diagnosa = outP[6];
+							}
+						}
 					}
 				}
 			}
-		}
-	}
 
-	cout << "\n Jumlah Pertanyaan Gejala = " << jmlPertanyaan
-			 << "\n\n" + garis + "\n\n" + hasilDiagnosaText + diagnosa + "\n\n" + garis;
+			cout << "\n Jumlah Pertanyaan Gejala = " << jmlPertanyaan
+					 << "\n\n" + garis + "\n\n" + hasilDiagnosaText + diagnosa + "\n\n" + garis;
+
+			ulangiBaru = '\0';
+			cout << "\n"
+					 << "Ulangi Lagi (Y/T) ? > ";
+			cin >> ulangi;
+
+			if (ulangi == 'y' || ulangi == 'Y')
+			{
+				cout << "Input Pasien Baru (Y/T) ? ";
+				cin >> ulangiBaru;
+				cin.ignore();
+
+				system("cls");
+			}
+			else
+				cout << "\n"
+						 << "Terima Kasih.";
+
+		} while (ulangiBaru == 't' || ulangiBaru == 'T');
+	} while (ulangiBaru == 'y' || ulangiBaru == 'Y');
 
 	return 0;
 }
