174755b7d230cac6a67257e7f0f5f3b5c9eb43d9
rafliyulistiawan <rafliyulistiawan@gmail.com>
2021-11-16 23:45:25 +0700

    add create feature

--- crudArray.cpp
+++ crudArray.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
+
+void readData(int length, string data[][3], string datafield[]);
 
 int main()
 {
 	string newData;
 	string menu[5] = {"[1] Read Data", "[2] Add Data", "[3] Edit Data", "[4] Delete Data", "[0] Exit"};
+	string dataField[3] = {"Nama", "Alamat", "Umur"};
 	int i, arrLength, found, dataAdded = 0, dataEdited;
 	char inMenu;
 
@@ -20,7 +24,7 @@ int main()
 
 	} while (arrLength <= 2);
 
-	string data[arrLength];
+	string data[arrLength][3];
 
 	do
 	{
@@ -30,90 +34,91 @@ int main()
 
 		cout << "Choose > ";
 		cin >> inMenu;
+		cin.ignore();
 
 		if (inMenu == '1')
 		{
-			cout << "Data saat ini: \n";
-			for (i = 0; i < arrLength; i++)
-			{
-				string res = (data[i] == "") ? "[Data tidak ada]." : data[i];
-				cout << i + 1 << ". " + res + "\n";
-			}
+			readData(arrLength, data, dataField);
 		}
 
 		if (inMenu == '2')
 		{
 			if (dataAdded < arrLength)
 			{
-				cout << "Masukkan data > ";
-				cin >> newData;
 
-				data[dataAdded] = newData;
+				for (i = 0; i < 3; i++)
+				{
+					cout << dataField[i] + ": ";
+					getline(cin, data[dataAdded][i]);
+				}
+
 				dataAdded++;
 				cout << "Data Added! \n";
+
+				readData(arrLength, data, dataField);
 			}
 			else
 				cout << "Data telah melebihi kapasitas! \n";
 		}
 
-		if (inMenu == '3')
-		{
-			found = 0;
-			for (i = 0; i < arrLength; i++)
-			{
-				if (data[i] != "")
-					found++;
-			}
+		// if (inMenu == '3')
+		// {
+		// 	found = 0;
+		// 	for (i = 0; i < arrLength; i++)
+		// 	{
+		// 		if (data[i] != "")
+		// 			found++;
+		// 	}
 
-			if (found >= 1)
-			{
-				do
-				{
-					cout << "Data ke: ";
-					cin >> dataEdited;
+		// 	if (found >= 1)
+		// 	{
+		// 		do
+		// 		{
+		// 			cout << "Data ke: ";
+		// 			cin >> dataEdited;
 
-					if (dataEdited == 0)
-						cout << "Data tidak ditemukan. \n";
+		// 			if (dataEdited == 0)
+		// 				cout << "Data tidak ditemukan. \n";
 
-				} while (dataEdited == 0);
+		// 		} while (dataEdited == 0);
 
-				cout << "Data baru: ";
-				cin >> newData;
+		// 		cout << "Data baru: ";
+		// 		cin >> newData;
 
-				data[dataEdited - 1] = newData;
-				cout << "Data Edited! \n";
-			}
-			else
-				cout << "Data tidak ada. \n";
-		}
+		// 		data[dataEdited - 1] = newData;
+		// 		cout << "Data Edited! \n";
+		// 	}
+		// 	else
+		// 		cout << "Data tidak ada. \n";
+		// }
 
-		if (inMenu == '4')
-		{
-			found = 0;
-			for (i = 0; i < arrLength; i++)
-			{
-				if (data[i] != "")
-					found++;
-			}
+		// if (inMenu == '4')
+		// {
+		// 	found = 0;
+		// 	for (i = 0; i < arrLength; i++)
+		// 	{
+		// 		if (data[i] != "")
+		// 			found++;
+		// 	}
 
-			if (found >= 1)
-			{
-				do
-				{
-					cout << "Data ke: ";
-					cin >> dataEdited;
+		// 	if (found >= 1)
+		// 	{
+		// 		do
+		// 		{
+		// 			cout << "Data ke: ";
+		// 			cin >> dataEdited;
 
-					if (dataEdited == 0 || data[dataEdited - 1] == "")
-						cout << "Data tidak ditemukan. \n";
+		// 			if (dataEdited == 0 || data[dataEdited - 1] == "")
+		// 				cout << "Data tidak ditemukan. \n";
 
-				} while (dataEdited == 0 || data[dataEdited - 1] == "");
+		// 		} while (dataEdited == 0 || data[dataEdited - 1] == "");
 
-				data[dataEdited - 1] = "";
-				cout << "Data Deleted!";
-			}
-			else
-				cout << "Data tidak ada. \n";
-		}
+		// 		data[dataEdited - 1] = "";
+		// 		cout << "Data Deleted!";
+		// 	}
+		// 	else
+		// 		cout << "Data tidak ada. \n";
+		// }
 
 		if (inMenu > '4')
 			cout << "Menu tidak ada! \n";
@@ -123,4 +128,39 @@ int main()
 
 	cout << "Terima kasih.";
 	return 0;
+}
+
+void readData(int length, string data[][3], string dataField[])
+{
+	int i;
+	// Mencari jumlah kata terbanyak di setiap field
+	int totalColLength, colLength[3] = {4, 6, 4};
+	for (i = 0; i < length; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			int dataLength = data[i][j].length();
+			colLength[j] = (dataLength > colLength[j]) ? dataLength : colLength[j];
+		}
+	}
+	totalColLength = colLength[0] + colLength[1] + colLength[2];
+
+	cout << "Data saat ini: \n";
+	cout << "| No | ";
+	for (i = 0; i < 3; i++)
+		cout << left << setw(colLength[i] + 1) << dataField[i] << "| ";
+
+	cout << "\n";
+
+	for (i = 0; i < length; i++)
+	{
+		cout << "| " << i + 1 << "  | ";
+		for (int j = 0; j < 3; j++)
+		{
+			string res = (data[i][j] == "") ? "-" : data[i][j];
+			string separator = (j >= 3) ? "|" : "| ";
+			cout << left << setw(colLength[j] + 1) << res << separator;
+		}
+		cout << "\n";
+	}
 }
