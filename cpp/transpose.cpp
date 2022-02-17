68a1be9b19db66081a9ddd0d8fac00a98af46ded
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-02-17 09:07:06 +0700

    change matrix size to be more dynamic

--- cpp/transpose.cpp
+++ cpp/transpose.cpp
@@ -4,20 +4,48 @@ using namespace std;
 
 int main()
 {
-	int a[2][3] = {
-			{2, 3, 6},
-			{4, 7, 8}};
+	int rowSize, colSize;
 
-	int bt[3][2];
+	cout << "Masukkan jumlah baris matriks A: ";
+	cin >> rowSize;
+	cout << "Masukkan jumlah kolom matriks A: ";
+	cin >> colSize;
+	cout << "\n";
 
-	for (int i = 0; i < 3; i++)
+	int a[rowSize][colSize],
+			transposed[colSize][rowSize];
+
+	for (int i = 0; i < rowSize; i++)
 	{
-		for (int j = 0; j < 2; j++)
+		for (int j = 0; j < colSize; j++)
 		{
-			bt[i][j] = a[j][i];
-			cout << bt[i][j] << " ";
+			cout << "Masukkan matriks A[" << i + 1 << "][" << j + 1 << "]: ";
+			cin >> a[i][j];
 		}
 		cout << "\n";
+	}
+	system("CLS");
+
+	cout << "Matriks A[" << rowSize << "][" << colSize << "]: \n";
+	for (int i = 0; i < rowSize; i++)
+	{
+		cout << "[ ";
+		for (int j = 0; j < colSize; j++)
+			cout << a[i][j] << " ";
+		cout << "] \n";
+	}
+	cout << "\n";
+
+	cout << "Hasil transpose matriks A[" << colSize << "][" << rowSize << "]: \n";
+	for (int i = 0; i < colSize; i++)
+	{
+		cout << "[ ";
+		for (int j = 0; j < rowSize; j++)
+		{
+			transposed[i][j] = a[j][i];
+			cout << transposed[i][j] << " ";
+		}
+		cout << "] \n";
 	}
 
 	return 0;
