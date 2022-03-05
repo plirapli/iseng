836808d69dfd1f6bcc2d2b19a2a8e274876e5c20
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-03-05 14:19:51 +0700

    edit addrekaman

--- cpp/linearSearch.cpp
+++ cpp/linearSearch.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -10,94 +9,141 @@ struct Penduduk
 	char gol;
 };
 
-void addRekaman(Penduduk penduduk[]);
-void readRekaman(Penduduk penduduk[]);
-void readRekaman(Penduduk profil[], int index);
-int lsRekaman(Penduduk penduduk[], int size, int input);	// Linear Search
-int lssRekaman(Penduduk penduduk[], int size, int input); // Linear Sentinel Search
+void addRekaman(Penduduk penduduk[], int &n);
+void readRekaman(Penduduk penduduk[], int n);
+void readProfil(Penduduk profil[], int index);
+int lsRekaman(Penduduk penduduk[], int n, int input);	 // Linear Search
+int lssRekaman(Penduduk penduduk[], int n, int input); // Linear Sentinel Search
 
 int main()
 {
 	Penduduk rekaman[10];
-	int noInduk, lsIndex, lssIndex;
-	int arrSize = sizeof(rekaman) / sizeof(rekaman[0]);
+	int pendudukAktif = 0;
+	char inputMenu;
 
-	addRekaman(rekaman);
-	readRekaman(rekaman);
+	do
+	{
+		cout << "Menu: \n"
+				 << "[1] Tambah Penduduk \n"
+				 << "[2] Lihat Daftar Penduduk \n"
+				 << "[3] Cari Penduduk \n"
+				 << "[Lainnya] Keluar \n"
+				 << "Pilih > ";
+		cin >> inputMenu;
+		system("CLS");
 
-	cout << "Cari Nomor Induk: ";
-	cin >> noInduk;
+		switch (inputMenu)
+		{
+		case '1':
+			addRekaman(rekaman, pendudukAktif);
+			break;
 
-	lsIndex = lsRekaman(rekaman, arrSize, noInduk);
-	lssIndex = lssRekaman(rekaman, arrSize, noInduk);
+		case '2':
+			readRekaman(rekaman, pendudukAktif);
+			break;
 
-	cout << "Hasil pencarian penduduk dengan NI: " << noInduk << "\n";
+		case '3':
+		{
+			int noInduk, lsIndex, lssIndex;
 
-	cout << "1. Linear Search: ";
-	if (lsIndex != -1)
-		readRekaman(rekaman, lsIndex);
-	else
-		cout << "Tidak ditemukan. \n\n";
+			readRekaman(rekaman, pendudukAktif);
 
-	cout << "2. Linear Sentinel Search: ";
-	if (lssIndex != -1)
-		readRekaman(rekaman, lssIndex);
-	else
-		cout << "Tidak ditemukan. \n\n";
+			cout << "Cari Nomor Induk: ";
+			cin >> noInduk;
+
+			lsIndex = lsRekaman(rekaman, pendudukAktif, noInduk);
+			lssIndex = lssRekaman(rekaman, pendudukAktif, noInduk);
+
+			cout << "Hasil pencarian penduduk dengan NI: " << noInduk << "\n";
+
+			cout << "1. Linear Search: ";
+			if (lsIndex != -1)
+				readRekaman(rekaman, lsIndex);
+			else
+				cout << "Tidak ditemukan. \n\n";
+
+			cout << "2. Linear Sentinel Search: ";
+			if (lssIndex != -1)
+				readRekaman(rekaman, lssIndex);
+			else
+				cout << "Tidak ditemukan. \n\n";
+
+			break;
+		}
+
+		default:
+			inputMenu = -1;
+			break;
+		}
+	} while (inputMenu != -1);
 
 	cout << "Terima kasih.";
 
 	return 0;
 }
 
-void readRekaman(Penduduk penduduk[])
+void addRekaman(Penduduk penduduk[], int &n)
+{
+	cout << "Tambah Penduduk \n";
+	cout << "Nomor Induk: ";
+	cin >> penduduk[n].noInduk;
+
+	cout << "Nama: ";
+	cin.ignore();
+	getline(cin, penduduk[n].nama);
+
+	cout << "Alamat: ";
+	getline(cin, penduduk[n].alamat);
+
+	cout << "Gol: ";
+	cin >> penduduk[n].gol;
+
+	cout << "Penduduk berhasil ditambahkan. \n\n";
+
+	n++;
+}
+
+void readRekaman(Penduduk penduduk[], int n)
 {
 	cout << "Daftar penduduk: \n";
-	for (int i = 0; i < 10; i++)
+	if (n > 0)
 	{
-		cout << i + 1 << ". [" << penduduk[i].noInduk << "] "
-				 << penduduk[i].nama << ", " << penduduk[i].gol
-				 << " (" << penduduk[i].alamat << ") \n";
+		for (int i = 0; i < n; i++)
+		{
+			cout << i + 1 << ". [" << penduduk[i].noInduk << "] "
+					 << penduduk[i].nama << ", " << penduduk[i].gol
+					 << " (" << penduduk[i].alamat << ") \n";
+		}
 	}
+	else
+		cout << "Penduduk belum ada. \n";
+
 	cout << "\n";
 }
 
-void readRekaman(Penduduk profil[], int index)
+void readProfil(Penduduk profil[], int index)
 {
 	cout << "[" << profil[index].noInduk << "] "
 			 << profil[index].nama << ", " << profil[index].gol
 			 << " (" << profil[index].alamat << ") \n\n";
 }
 
-void addRekaman(Penduduk penduduk[])
+int lsRekaman(Penduduk penduduk[], int n, int input)
 {
-	for (int i = 0; i < 10; i++)
-	{
-		penduduk[i].noInduk = 123210010 + i;
-		penduduk[i].nama = "Orang " + to_string(i + 1);
-		penduduk[i].alamat = "Alamat " + to_string(i + 1);
-		penduduk[i].gol = 65 + i;
-	}
-}
-
-int lsRekaman(Penduduk penduduk[], int size, int input)
-{
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < n; i++)
 		if (input == penduduk[i].noInduk)
 			return i;
 	return -1;
 }
 
-int lssRekaman(Penduduk penduduk[], int size, int input)
+int lssRekaman(Penduduk penduduk[], int n, int input)
 {
-	int last = penduduk[size - 1].noInduk, i = 0;
+	int last = penduduk[n - 1].noInduk, i = 0;
 
-	penduduk[size - 1].noInduk = input;
-
+	penduduk[n - 1].noInduk = input;
 	while (input != penduduk[i].noInduk)
 		i++;
+	penduduk[n - 1].noInduk = last;
 
-	penduduk[size - 1].noInduk = last;
-
-	return (i < size - 1) || (input == penduduk[size - 1].noInduk) ? i : -1;
+	return (i < n - 1) || (input == penduduk[n - 1].noInduk) ? i : -1;
 }
