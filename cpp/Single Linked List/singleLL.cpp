e7e71cf70eb4dbc6d5457c22eae1440583c4c644
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-08-16 19:51:43 +0700

    add latian single linked list

---
+++ cpp/Single Linked List/singleLL.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+struct Mhs
+{
+  string nama;
+  int umur;
+
+  Mhs *next;
+} * head, *tail, *cur, *newNode;
+
+void createSingleLinkedList(string nama, int umur)
+{
+  head = new Mhs();
+  head->nama = nama;
+  head->umur = umur;
+
+  head->next = NULL;
+  tail = head;
+}
+
+void addFirst(string nama, int umur)
+{
+  newNode = new Mhs();
+  newNode->nama = nama;
+  newNode->umur = umur;
+
+  newNode->next = head;
+  head = newNode;
+};
+
+void printSingleLinkedList()
+{
+  cur = head;
+
+  while (cur != NULL)
+  {
+    cout << "Nama: " << cur->nama << "\n";
+    cout << "Tgl Lahir: " << cur->umur << "\n\n";
+
+    cur = cur->next;
+  }
+}
+
+int main()
+{
+  createSingleLinkedList("Muhammad Rafli", 21);
+  addFirst("Seva", 19);
+  addFirst("Giantama", 21);
+
+  printSingleLinkedList();
+
+  return 0;
+}
