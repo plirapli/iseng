590e357e35065864e4598b13003538dad518a93c
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-08-17 15:25:26 +0700

    add add last double linked list

--- cpp/Linked List/doubleLinkedList.cpp
+++ cpp/Linked List/doubleLinkedList.cpp
@@ -14,7 +14,7 @@ struct Mhs
 // Create
 void create(string nama, int umur);   // Create first node
 void addFirst(string nama, int umur); // Add node to first
-// void addLast(string nama, int umur);               // Add node to last
+void addLast(string nama, int umur);  // Add node to last
 // void addMiddle(string nama, int umur, int posisi); // Add node to nth list
 
 // // Delete
@@ -35,13 +35,13 @@ int main()
 {
   create("Rafli", 21);
   addFirst("Seva", 19);
-  // addLast("Farel", 18);
+  addLast("Farel", 18);
   // delFirst();
   // delLast();
   // addLast("Giantama", 21);
 
   readNodes();
-  // Seva, Rafli
+  // Seva, Rafli, Farel
 
   // changeFirst("Seva", 24);
   // changeLast("Syaikul", 17);
@@ -74,9 +74,21 @@ void addFirst(string nama, int umur)
   newNode->umur = umur;
 
   newNode->next = headNode; // Next node baru menunjuk node pertama
-  newNode->prev = NULL;     // Prev node baru menunjuk ke NULL
-  headNode->prev = newNode; // Prev node pertama menunjuk ke node baru
+  newNode->prev = NULL;     // Prev node baru menunjuk NULL
+  headNode->prev = newNode; // Prev node pertama menunjuk node baru
   headNode = newNode;       // Head pindah ke node baru
+}
+
+void addLast(string nama, int umur)
+{
+  newNode = new Mhs();
+  newNode->nama = nama;
+  newNode->umur = umur;
+
+  newNode->prev = tailNode; // Prev node baru menunjuk node terakhir
+  newNode->next = NULL;     // Next node baru menunjuk NULL
+  tailNode->next = newNode; // Next node terakhir menunjuk node baru
+  tailNode = newNode;       // Tail pindah ke node baru
 }
 
 // void addMiddle(string nama, int umur, int posisi)
@@ -103,17 +115,6 @@ void addFirst(string nama, int umur)
 //     newNode->next = curNode->next;
 //     curNode->next = newNode;
 //   }
-// }
-
-// void addLast(string nama, int umur)
-// {
-//   newNode = new Mhs();
-//   newNode->nama = nama;
-//   newNode->umur = umur;
-
-//   newNode->next = NULL;
-//   tailNode->next = newNode;
-//   tailNode = newNode;
 // }
 
 // void changeFirst(string nama, int umur)
