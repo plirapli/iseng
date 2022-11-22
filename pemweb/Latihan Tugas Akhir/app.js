a7d69416e5ad998aa70a7cb4e6e755ceda2ba3a1
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-11-22 10:37:42 +0700

    add latihan node js

---
+++ pemweb/Latihan Tugas Akhir/app.js
@@ -0,0 +1,50 @@
+// Import Modul
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || '3001';
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+// Untuk keperluan upload data dari form
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+app.get('/contoh-2/:num1/:num2', function (req, res) {
+  const num1 = parseInt(req.params.num1);
+  const num2 = parseInt(req.params.num2);
+  const hasil = num1 + num2;
+
+  res.render('contoh_2', { num1, num2, hasil });
+});
+
+// menanpilkan contoh form
+app.get('/contoh-form', (req, res) => {
+  res.render('contoh_form');
+});
+
+// memproses form
+app.post('/proses-form', (req, res) => {
+  console.log(req.body);
+
+  // kirim data form ke UI
+  res.render('proses_form', {
+    nama: req.body.nama,
+    alamat: req.body.alamat,
+    kota: req.body.kota,
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server jalan di port ' + port);
+});
