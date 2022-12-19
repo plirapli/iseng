4e7bf480b6f28633d045e262a423427618acba2b
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-12-19 10:19:20 +0700

    change fetch assoc to object

--- pemweb/Latihan UTS + UAS/uas.php
+++ pemweb/Latihan UTS + UAS/uas.php
@@ -1,36 +1,39 @@
 <?php
-echo "<b>UAS</b> <a href='uts.php'>UTS</a> <br><br>";
-echo "UAS 2021-2022 <br>";
+echo "<b>UAS</b> <a href='uts.php'>UTS</a>" .
+  "<h2>UAS 2021-2022</h2>";
 
 $connect = new mysqli('localhost', 'root', '', 'nwind');
 
-$sql_1 = 'SELECT p.ProductName AS product, s.CompanyName AS supplier FROM products p 
-        INNER JOIN suppliers s ON p.SupplierID = s.SupplierID';
-$query_1 = $connect->query($sql_1);
+// NO. 1
+echo "1. Tuliskanlah kode program PHP sesingkat mungkin untuk membuat web yang akan menampilkan daftar nama products dan nama suppliers nya dari database NWIND ! <br><br>" .
+  "Jawab: <br>";
 
-echo "1. Tuliskanlah kode program PHP sesingkat mungkin untuk membuat web yang akan menampilkan daftar nama products dan nama suppliers nya dari database NWIND ! <br><br>";
-echo "Jawab: <br>";
-while ($row = $query_1->fetch_assoc()) {
-  echo "Product: " . $row['product'] . "<br>";
-  echo "Supplier: " . $row['supplier'] . "<br><br>";
+$sql_1 = 'SELECT p.ProductID AS id, p.ProductName AS product, s.CompanyName AS supplier FROM products p 
+          INNER JOIN suppliers s ON p.SupplierID = s.SupplierID';
+$result_1 = $connect->query($sql_1);
+while ($row = $result_1->fetch_object()) {
+  echo "Product: $row->product <br>";
+  echo "Supplier: $row->supplier <br>";
+  echo "<a href='uas_delete.php?id=$row->id'>Hapus</a> <br><br>";
 }
 echo "<br>";
 
-$sql_2 = "SELECT * FROM customers WHERE ContactName LIKE 'R%'";
-$query_2 = $connect->query($sql_2);
+// NO. 2
+echo "2. Tuliskanlah kode program PHP sesingkat mungkin untuk membuat web yang akan menampilkan daftar pelanggan dari database NWIND yang ContactName-nya diawali dengan huruf “R”! <br><br>" .
+  "Jawab: <br>";
 
-echo "2. Tuliskanlah kode program PHP sesingkat mungkin untuk membuat web yang akan menampilkan daftar pelanggan dari database NWIND yang ContactName-nya diawali dengan huruf “R”! <br><br>";
-echo "Jawab: <br>";
-while ($row = $query_2->fetch_assoc()) {
-  echo "ID: " . $row['CustomerID'] . "<br>";
-  echo "Company Name: " . $row['CompanyName'] . "<br>";
-  echo "Contact Name: " . $row['ContactName'] . "<br>";
-  echo "Contact Title: " . $row['ContactTitle'] . "<br>";
-  echo "Address: " . $row['Address'] . "<br>";
-  echo "City: " . $row['City'] . "<br>";
-  echo "Region: " . $row['Region'] . "<br>";
-  echo "Postal Code: " . $row['PostalCode'] . "<br>";
-  echo "Country: " . $row['Country'] . "<br>";
-  echo "Phone: " . $row['Phone'] . "<br>";
-  echo "Fax: " . $row['Fax'] . "<br><br>";
+$sql_2 = "SELECT * FROM customers WHERE ContactName LIKE 'R%'";
+$result_2 = $connect->query($sql_2);
+while ($row = $result_2->fetch_object()) {
+  echo "ID: $row->CustomerID <br>";
+  echo "Company Name: $row->CompanyName <br>";
+  echo "Contact Name: $row->ContactName <br>";
+  echo "Contact Title: $row->ContactTitle <br>";
+  echo "Address: $row->Address <br>";
+  echo "City: $row->City <br>";
+  echo "Region: $row->Region <br>";
+  echo "Postal Code: $row->PostalCode <br>";
+  echo "Country: $row->Country <br>";
+  echo "Phone: $row->Phone <br>";
+  echo "Fax: $row->Fax <br><br>";
 }
