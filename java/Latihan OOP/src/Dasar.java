17fe4828f030143803be859329b58dad9e06efe4
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-01-23 22:29:48 +0700

    add dasar java

---
+++ java/Latihan OOP/src/Dasar.java
@@ -0,0 +1,62 @@
+class Mahasiswa {
+  // Data Member
+  String nama, NIM;
+
+  // Constructor
+  Mahasiswa(String nama, String NIM) {
+    this.nama = nama;
+    this.NIM = NIM;
+  }
+
+  // Method w/o return & w/o param
+  void show() {
+    System.out.println("Halo " + this.nama + ", NIM " + this.NIM);
+  }
+
+  // Method w/o return & w/ param
+  void setNama(String nama) {
+    this.nama = nama;
+  }
+
+  // Method w/ return & w/o param
+  String getNama() {
+    return this.nama;
+  }
+
+  String getNIM() {
+    return this.NIM;
+  }
+
+  // Method w/ return & w/ param
+  String sayHi(String msg) {
+    return "Pesan: " + msg;
+  }
+}
+
+class Dosen {
+  String nama;
+
+  Dosen(String nama) {
+    this.nama = nama;
+  }
+}
+
+public class Dasar {
+  public static void main(String[] args) throws Exception {
+    Mahasiswa mhs1 = new Mahasiswa("Rafli", "123210078");
+
+    // Method
+    mhs1.show();
+    mhs1.setNama("Haikal");
+    mhs1.show();
+
+    System.out.println(mhs1.getNama());
+    System.out.println(mhs1.getNIM());
+
+    String msg = "This is a message.";
+    System.out.println(mhs1.sayHi(msg));
+
+    Dosen dosen1 = new Dosen("Norvido");
+    System.out.println(dosen1.nama);
+  }
+}
