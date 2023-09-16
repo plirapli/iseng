e6b50f1f88afa0b3a595cbef0813814c31f4e8dc
Rafli <rafliyulistiawan@gmail.com>
2023-09-16 11:40:24 +0700

    add file responsi

---
+++ js/latihan_itc/index.js
@@ -0,0 +1,19 @@
+// function tampilkanNilaiA() {
+//   console.log("Nilai A")
+// }
+
+// let tampilkanNilaiA = () => {
+//   console.log("Nilai A")
+// }
+
+// tampilkanNilaiA();
+
+// function tambahNilai(a, b) {
+//   return a + b;
+// }
+
+// let tambahNilai = (a, b) => {
+//   return a + b;
+// }
+
+// console.log(tambahNilai(5, 4))
